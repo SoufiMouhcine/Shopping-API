dbdfa17048ffab1edf5de8c3fec0ccd263d1d413
mouhcine_soufi <mohciness3@gmail.com>
2020-10-15 13:13:13 +0100

    sign in

--- routes/users.js
+++ routes/users.js
@@ -7,52 +7,90 @@ const bcrypt = require('bcrypt');
 /* GET users listing. */
 router.post('/signup', (req, res, next) => {
 
-  User.find({username:req.body.username})
-  .then(result=>{
-    if(result.length<1){
+  User.find({ username: req.body.username })
+    .then(result => {
+      if (result.length < 1) {
 
-      bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-          res.status(404).json({
-            message: err
-          })
-        } else {
-          const user = new User({
-            username: req.body.username,
-            password: hash
-          });
-          user.save()
-            .then(result => {
-              res.status(200).json({
-                message: result
-              })
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            res.status(404).json({
+              message: err
             })
-            .catch(err => {
-              res.status(404).json({
-                message: err
+          } else {
+            const user = new User({
+              username: req.body.username,
+              password: hash
+            });
+            user.save()
+              .then(result => {
+                res.status(200).json({
+                  message: result
+                })
               })
-            })
-        }
-      });
+              .catch(err => {
+                res.status(404).json({
+                  message: err
+                })
+              })
+          }
+        });
 
-    }
-    else{
-      res.status(404).json({
-        message : 'username already existe'
-      })
-    }
-    
-  })
-  .catch(err=>{
-    res.status(404).json({
-      message:err
+      }
+      else {
+        res.status(404).json({
+          message: 'username already existe'
+        })
+      }
+
     })
-  })
+    .catch(err => {
+      res.status(404).json({
+        message: err
+      })
+    })
 
 
 
 
 });
+
+
+router.get('/signin', (req, res, next) => {
+  User.find({ username: req.body.username })
+    .then(user => {
+      if (user.length >= 1) {
+        bcrypt.compare(req.body.password, user[0].password)
+          .then(result => {
+            if (result) {
+              res.status(200).json({
+                message: 'you are sign in'
+              })
+            }
+            else{
+              res.status(404).json({
+                message:'wrong passwrod'
+              })
+            }
+          })
+          .catch(err => {
+            res.status(404).json({
+              message: err
+            })
+          })
+
+      }
+      else {
+        res.status(404).json({
+          message: "we can't find user with this name"
+        })
+      }
+    })
+    .catch(err => {
+      res.status(404).json({
+        message: err
+      })
+    })
+})
 
 router.get('/', (req, res, next) => {
   const users = User.find({}, 'username password')
