987aa8480e2c49dfe1e1e63a47f88a83b3a062e8
mouhcine_soufi <mohciness3@gmail.com>
2020-10-15 01:13:52 +0100

    sign up validation + hash password

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
-const User=require('../models/User');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
-router.post('/signup', (req, res, next)=> {
-  const user = new User({
-    username : req.body.username,
-    password : req.body.password
-  });
-  user.save()
+router.post('/signup', (req, res, next) => {
+
+  User.find({username:req.body.username})
   .then(result=>{
-    res.status(200).json({
-      message : result
-    })
+    if(result.length<1){
+
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          res.status(404).json({
+            message: err
+          })
+        } else {
+          const user = new User({
+            username: req.body.username,
+            password: hash
+          });
+          user.save()
+            .then(result => {
+              res.status(200).json({
+                message: result
+              })
+            })
+            .catch(err => {
+              res.status(404).json({
+                message: err
+              })
+            })
+        }
+      });
+
+    }
+    else{
+      res.status(404).json({
+        message : 'username already existe'
+      })
+    }
+    
   })
   .catch(err=>{
     res.status(404).json({
-      message : err
+      message:err
     })
   })
+
+
+
+
 });
 
-router.get('/',(req,res,next)=>{
-  const users = User.find({},'username password')
-  .then(result=>{
-    res.status(200).json({
-      message:result
+router.get('/', (req, res, next) => {
+  const users = User.find({}, 'username password')
+    .then(result => {
+      res.status(200).json({
+        message: result
+      })
     })
-  })
-  .catch(err=>{
-    res.status(404).json({
-      message :err
+    .catch(err => {
+      res.status(404).json({
+        message: err
+      })
     })
-  })
 
 });
 
