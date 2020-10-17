c40137ba4e1b1945602632b7255116e1dc1b6f61
mouhcine_soufi <mohciness3@gmail.com>
2020-10-17 13:50:46 +0100

    GET and ADD new product

--- routes/products.js
+++ routes/products.js
@@ -27,12 +27,34 @@ router.get('/',(req,res,next)=>{
     Product.find()
     .then(result=>{
         res.status(200).json({
-            message : result
+            message : result.map(product=>{
+                return {
+                    _id:product._id,
+                    name:product.name,
+                    price :product.price,
+                    url :'http://localhost:2000/products/'+product._id
+                }
+
+            })
         })
     })
     .catch(err=>{
         res.status(404).json({
             message : err
+        })
+    })
+});
+
+router.get('/:id',(req,res,next)=>{
+    Product.findById(req.params.id,"name price" )
+    .then(result=>{
+        res.status(200).json({
+            message:result
+        })
+    })
+    .catch(err=>{
+        res.status(404).json({
+            message:err
         })
     })
 })
