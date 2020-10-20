ed3ad7563ce6274d6a33b54ff197e6a260aeb4d7
mouhcine_soufi <mohciness3@gmail.com>
2020-10-20 16:14:33 +0100

    delete order

--- routes/orders.js
+++ routes/orders.js
@@ -75,6 +75,20 @@ router.put('/:id', (req, res, next) => {
                 message: err
             })
         })
+});
+
+router.delete('/:id',(req,res,next)=>{
+    Order.deleteOne({_id:req.params.id})
+    .then(result=>{
+        res.status(204).json({
+            message:"order deleted successfully"
+        })
+    })
+    .catch(err=>{
+        res.status(404).json({
+            message:err
+        })
+    })
 })
 
 
